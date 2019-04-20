f6cb7316afeacd56ad6ec0d9a3dcdecdeee53570
shankyms <shankaranarayanan.m.s@gmail.com>
2019-04-20 09:26:17 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.scss";
+
+let HelloWorld = () => {
+  return <h1>Hello there World!</h1>
+}
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById("root")
+);
